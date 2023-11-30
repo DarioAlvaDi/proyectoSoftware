183fd5e49befde100924e5cb123bea86ff7cfb3f
Andrelyn12035 <kevo1803wow@gmail.com>
2023-11-30 07:16:15 -0600

    Avance detalles lugar

--- Implementacion/public/js/detalles_lugar.js
+++ Implementacion/public/js/detalles_lugar.js
@@ -14,7 +14,13 @@ function colorearf() {
     else (color.removeAttribute("fill"));
 }
 
-fetch('https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyAUvlCB-hBLbxN1Li2d8HwDANi4yLgmQ8I&location=-33.8670522,151.1957362&radius=5000&type=restaurant')
+fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key=AIzaSyAUvlCB-hBLbxN1Li2d8HwDANi4yLgmQ8I&location=-33.8670522,151.1957362&radius=5000&type=restaurant', {
+    method: 'GET',
+    headers: {
+        'Content-Type': 'text/plain',
+        'Access-Control-Allow-Origin': '*'
+    }
+})
     .then(data => {
         return data.json()
     }).then(jsonData => {
