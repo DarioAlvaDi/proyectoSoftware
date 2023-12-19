67e11ac9c03c0c20f884c6b97d8ae7e930be8051
Andrelyn12035 <kevo1803wow@gmail.com>
2023-12-19 08:34:09 -0600

    Itinerario

--- Implementacion/public/js/itinerarioDia.js
+++ Implementacion/public/js/itinerarioDia.js
@@ -283,3 +283,216 @@ function imprimir(elemento) {
         });
     }
 }
+
+
+window.onload = function () {
+    mostrarItinerario();
+};
+function mostrarItinerario() {
+    let ubi = JSON.parse(localStorage.getItem("ubi"));
+    const urlParams = new URLSearchParams(window.location.search);
+
+    const id = urlParams.get("id");
+    let data = { id: id };
+    fetch("/turistas/consultarItinerarioFecha", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (myJson) {
+            console.log(myJson);
+            const contenido = document.getElementById('contenido');
+            if (myJson.length === 0) {
+                contenido.innerHTML = '';
+                contenido.innerHTML =
+                    `
+                    <div class="tab-container">
+                    <div
+                      class="tab-content d-flex justify-content-center align-items-center"
+                    >
+                      <div
+                        class="container d-flex justify-content-center align-items-center"
+                      >
+                        <div class="col-12 favoritos-container">
+                          <div class="tab-shadow">
+                            <div class="favoritos-empty">
+                              <p style="margin-left: 20px">
+                                Itinerario vacío
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div> 
+                    <div style="height: 10rem"></div>
+                <footer class="fixed-bottom navColor">
+                    <!--Opciones de agregar y borrar-->
+                    <div class="container-fluid" style="margin-bottom: 1.5rem">
+                        <div class="text-center">
+                            <button type="button" class="btn ">
+                                <img src="../imgs/itinerarioDia/Agregar.png" alt="agregar">
+                            </button>
+                            
+                        </div>
+        
+                    </div>
+                    
+                </footer> 
+                    `;
+            } else {
+                const urlParams = new URLSearchParams(window.location.search);
+
+                const id = urlParams.get('id')
+                contenido.innerHTML = '';
+                contenido.innerHTML = `
+                <div class="container">
+                    <div id="elemento"></div>        
+                </div>
+                
+                    `+ `
+                  
+                <div style="height: 10rem"></div>
+                <footer class="fixed-bottom navColor">
+                    <!--Opciones de agregar y borrar-->
+                    <div class="container-fluid" style="margin-bottom: 1.5rem">
+                        <div class="text-center">
+                            <button type="button" class="btn ">
+                                <img src="../imgs/itinerarioDia/Agregar.png" alt="agregar">
+                            </button>
+                            
+                        </div>
+        
+                        <div class="row">
+        
+                            <div class="col text-center">
+                                <button type="button" class="btn btn-danger rounded-4" data-bs-toggle="modal" data-bs-target="#modalItinerarios" id="eliminarbtn">
+                                        <img src="../imgs/itinerarioDia/BoteBasura.png" alt="eliminar">
+                                        Eliminar Itinerario
+                                </button>
+                            </div>
+        
+        
+                            <div class="col text-center">
+                                <button type="button" class="btn btn-dark rounded-4">
+                                    <a href="/turistas/test?id=${id}">Generar ruta</a>
+                                </button>
+        
+                            </div>
+        
+                        </div>
+                    </div>
+                    
+                </footer>
+            `;
+                let num = 0;
+                myJson.forEach(item => {
+                    console.log(item);
+
+                    service = new google.maps.places.PlacesService(
+                        document.createElement("div")
+                    );
+                    var request = {
+                        placeId: item.Id_Lugar,
+                        language: "es",
+                        fields: ["name", "photo"]
+                    };
+
+                    service.getDetails(request, callback);
+                    function callback(place, status) {
+                        console.log(place);
+                        let src = "";
+                        if (status == google.maps.places.PlacesServiceStatus.OK) {
+                            let nombreLugar = place.name;
+                            let nuevoElemento = document.createElement("div");
+                            nuevoElemento.classList.add('resize');
+                            nuevoElemento.id = item.Id_Itinerario;
+                            if (Object.keys(place).includes("photos")) {
+                                src = place.photos[0].getUrl({ maxWidth: 300, maxHeight: 400 });
+                            }
+                            nuevoElemento.innerHTML = `
+                <div class="container border border-3 border-dark p-3 m-3 mx-auto sitioColor" style="border-radius: 24px">
+                        <div class="row">
+                            <div class="col text-center">
+                                <img src="${src}" alt="busqueda">
+                            </div>
+            
+                            <div class="col">
+                                <div class="row text-center">
+                                    <h6 class="fw-bold"> `+ nombreLugar + `</h6>
+                                </div>
+            
+                                <div class="row text-center d-inline-flex">
+            
+                                    <div class="col">
+                                        <input type="time" name="hora" id="horaElemento`+ num + `" class="form-select w-auto" value="${item.Hora_Itinerario}">
+                                    </div>
+            
+                                    <div class="col">
+                                        <select name="" id="visitaElemento`+ num + `"  class="form-select text-bg-light form-select-sm w-auto">
+                                            <option selected value="0">No visitado</option>
+                                            <option value="1">Visitado</option>
+                                        </select>
+                                    </div>
+            
+                                    <div class="col">
+            
+                                        <button type="button" class="btn" data-bs-toggle="modal" data-bs-target="#modalElemento`+ num + `">
+                                            <img src="../imgs/itinerarioDia/BotonEliminar.png" alt="botonEliminar">
+            
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                </div>
+            
+                <!-- Confirmacion eliminar elemento -->
+                    <div class="modal fade" id="modalElemento`+ num + `" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+                        <div class="modal-dialog modal-dialog-centered">
+                        <div class="modal-content navColor">
+            
+                            <button type="button" class="btn-close m-2" data-bs-dismiss="modal" aria-label="Close"></button>
+            
+                            <div class="modal-body text-center">
+                                <div class="row">
+                                    <h5>¿Estás seguro de que deseas eliminar este elemento de tu itinerario?</h5>
+            
+                                </div>
+            
+                                <div class="row m-3">
+                                    <button type="button"  data-bs-toggle="modal" data-bs-target="#modalElemento" class="btn btn-danger rounded-4" id="eliminar`+ num + `">
+                                        <img src="../imgs/itinerarioDia/BoteBasura.png" alt="eliminar">
+                                        Confirmar eliminación
+                                    </button>
+                                </div>
+            
+                                <div class="row m-3">
+                                    <button type="button" class="btn btn-secondary rounded-4" data-bs-toggle="modal" data-bs-target="#modalElemento">
+                                        <img src="../imgs/itinerarioDia/BotonEliminar.png" alt="eliminar">
+                                        Cancelar
+                                    </button>
+            
+                                </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+            
+                `;
+
+                            document.getElementById("elemento").appendChild(nuevoElemento);
+                            num++;
+                        }
+                    }
+                });
+
+            }
+
+        });
+}
